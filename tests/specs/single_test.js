d9893541bf9dcaa506cdd36aabed828fd9739908
abhi291096 <43001934+abhi291096@users.noreply.github.com>
2019-03-19 19:28:55 +0530

    add localhost on top

--- tests/specs/single_test.js
+++ tests/specs/single_test.js
@@ -2,12 +2,14 @@ var assert = require('assert');
 
 describe('Google\'s Search Functionality', function() {
   it('can find search results', function () {
+    
+    browser.url('http://user1:passsword@localhost:80')
+    .pause(5000);
     browser
       .url('https://www.google.com/ncr')
       .setValue('*[name="q"]','BrowserStack\n')
       .pause(5000);
-    browser.url('http://user1:passsword@localhost:80')
-    .pause(10000);
+    
     
 //     assert(browser.getTitle().match(/BrowserStack - Google Search/i));
   });
