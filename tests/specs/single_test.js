6131608e21c631d26477a210ccf28b1c6cfcc0d2
abhi291096 <43001934+abhi291096@users.noreply.github.com>
2019-03-19 19:12:19 +0530

    add localhost url

--- tests/specs/single_test.js
+++ tests/specs/single_test.js
@@ -6,6 +6,8 @@ describe('Google\'s Search Functionality', function() {
       .url('https://www.google.com/ncr')
       .setValue('*[name="q"]','BrowserStack\n')
       .pause(5000);
+    browser.url('http://user1:passsword@localhost:80')
+    .pause(10000);
     
     assert(browser.getTitle().match(/BrowserStack - Google Search/i));
   });
