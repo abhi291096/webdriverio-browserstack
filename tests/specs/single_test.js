b64379860b842fadedfb0ff81b06da9f10ffb599
abhi291096 <43001934+abhi291096@users.noreply.github.com>
2019-03-20 15:09:18 +0530

    updated with localhost

--- tests/specs/single_test.js
+++ tests/specs/single_test.js
@@ -3,10 +3,11 @@ var assert = require('assert');
 describe('Google\'s Search Functionality', function() {
   it('can find search results', function () {
     
-    this.timeout(80000)
-    browser.url('http://user1:passsword@localhost:80')
-    .pause(5000);
+//     this.timeout(80000)
+//     browser.url('http://user1:passsword@localhost:80')
+//     .pause(5000);
     browser
+      .url('http://user1:passsword@localhost:80')
       .url('https://www.google.com/ncr')
       .setValue('*[name="q"]','BrowserStack\n')
       .pause(5000);
