de5313c7741a7cc41caa4a74c8da988f7b443dff
abhi291096 <43001934+abhi291096@users.noreply.github.com>
2019-03-20 15:05:07 +0530

    add timeout

--- tests/specs/single_test.js
+++ tests/specs/single_test.js
@@ -3,6 +3,7 @@ var assert = require('assert');
 describe('Google\'s Search Functionality', function() {
   it('can find search results', function () {
     
+    this.timeout(80000)
     browser.url('http://user1:passsword@localhost:80')
     .pause(5000);
     browser
